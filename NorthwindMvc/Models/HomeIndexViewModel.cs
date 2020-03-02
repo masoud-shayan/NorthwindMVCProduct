7cdb60460b9109c22e41f8e1e8497e1218dcdeeb
masoud <masoud.shayannejad@gmail.com>
2020-03-02 19:13:42 +0330

    first commit

---
+++ NorthwindMvc/Models/HomeIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using NorthwindEntitiesLib;
+
+namespace NorthwindMvc.Models
+{
+    public class HomeIndexViewModel
+    {
+        public int VisitorCount { get; set; }
+        public IList<Product> Products { get; set; }
+        public IList<Category> Categories { get; set; }
+    }
+}
---
+++ NorthwindMvc/Views/Home/ProductDetail.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NorthwindMvc.Views.Home
+{
+    public class ProductDetail : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
